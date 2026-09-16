32ea8dbb9f64ff1100841d00ffa50f75551421f9
savannahpellot <savannahpellot09@gmail.com>
2026-09-16 18:06:47 -0400

    Update Assignment1Program1.java

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -3,3 +3,14 @@ public class Assignment1Program1 {
 
   }
 }
+}
+public class program1 {
+  public static void main(String[] args) {
+    System.out.println(" SSSSSSSSS   SSSSSSSSS");
+    System.out.println("SSSS         SS     SS");
+    System.out.println(" SSSSSSSS    SSSSSSSSS");
+    System.out.println("      SSSS   SS       ");
+    System.out.println("SSSSSSSS     SS.      ");
+    System.out.println();
+  }
+}
